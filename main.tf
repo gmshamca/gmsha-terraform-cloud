c0c17fbc92ab1795037d369d20d5637aefa80238
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-05-19 19:50:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ variable "subnet_id" {}
 resource "aws_instance" "terraform-cloud-demo" {
   ami= var.ami
   instance_type = var.instance_type
+  subnet_id= var.subnet_id
   tags={
     name="terraform-cloud-demo"
   }
